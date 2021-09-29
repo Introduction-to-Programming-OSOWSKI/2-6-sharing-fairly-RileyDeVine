0e9a2a2ce9605164ba79c8c9fefff82ab30bcb83
rileydevine <824055@mystma.org>
2021-09-29 09:41:02 -0500

    submitted

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 #WRITE YOUR CODE IN THIS FILE
+#define share fair
+def shareFair(x,y):
+    
+    #if no remainder
+    if (x%y) == 0:
+        return True
+
+    #if there is remainder
+    else:
+        return False
+
+    #print share fair
+print(shareFair(10, 5))
